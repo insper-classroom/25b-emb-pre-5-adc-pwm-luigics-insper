e994cb3f7ca454c3553c9911a517ad784830f9a3
Luigi <luigics@al.insper.edu.br>
2025-09-22 08:59:46 -0300

    exe2

--- exe2/main.c
+++ exe2/main.c
@@ -8,19 +8,28 @@
 #include "hardware/pwm.h"
 
 const int PWM_0_PIN = 4;
+const int PWM_1_PIN = 6;
 
 void led_task(void *p) {
+    //led vermelho
     gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
-    pwm_set_clkdiv(slice_num, 125);
-    pwm_set_wrap(slice_num, 100);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 80);
-    pwm_set_enabled(slice_num, true);
+    uint slice_num_r = pwm_gpio_to_slice_num(PWM_0_PIN);
+    pwm_set_clkdiv(slice_num_r, 125);
+    pwm_set_wrap(slice_num_r, 100);
+    pwm_set_chan_level(slice_num_r, PWM_CHAN_A, 80);
+    pwm_set_enabled(slice_num_r, true);
+
+    //led verde
+    gpio_set_function(PWM_1_PIN, GPIO_FUNC_PWM);
+    uint slice_num_g = pwm_gpio_to_slice_num(PWM_1_PIN);
+    pwm_set_clkdiv(slice_num_g, 125);
+    pwm_set_wrap(slice_num_g, 100);
+    pwm_set_chan_level(slice_num_g, PWM_CHAN_B, 20);
+    pwm_set_enabled(slice_num_g, true);
 
     while (true) {
     }
 }
-
 int main() {
     stdio_init_all();
     printf("Start RTOS \n");
