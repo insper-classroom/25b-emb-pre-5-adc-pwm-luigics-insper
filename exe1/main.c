c9bbab37a05a65cb2467251ab40b31ad6506d5d8
Luigi <luigics@al.insper.edu.br>
2025-09-22 08:52:08 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -17,8 +17,9 @@ void adc_task(void *p) {
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
 
-    uint16_t result;
     while (1) {
+        uint16_t result;
+
         adc_select_input(1);
         result = adc_read();
         printf("voltage 1: %f V\n", result * conversion_factor);
